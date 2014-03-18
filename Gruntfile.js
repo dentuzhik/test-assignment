0e45b07990e5f9b9177c5f1764db98dbd158fb5b
Denis Tuzhik <dentuzhik@gmail.com>
2014-03-18 13:07:27 +0300

    Set up basic development grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,63 @@
+'use strict';
 
 module.exports = function(grunt) {
   // Loading all the grunt tasks, specified in package.json file
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
+    config: {
+      paths: {
+        app: 'app',
+        dist: 'dist'
+      },
+      connect: {
+        hostname: 'localhost',
+        port: 9000,
+        base: 'app'
+      }
+    },
 
+    connect: {
+      server: {
+        options: {
+          hostname: '<%= config.connect.hostname %>',
+          port: '<%= config.connect.port %>',
+          base: '<%= config.connect.base %>',
+          debug: true,
+          livereload: true
+        }
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      html: {
+        files: '<%= config.paths.app %>/*.html'
+      },
+      scripts: {
+        files: '<%= config.paths.app %>/scripts/**/*.js',
+        tasks: [],
+      },
+      styles: {
+        files: '<%= config.paths.app %>/styles/**/*.css',
+        tasks: []
+      }
+    },
+
+    open: {
+      dev: {
+        path: 'http://<%= config.connect.hostname %>:<%= config.connect.port %>'
+      }
+    }
   });
+
+  grunt.registerTask('dev', [
+    'connect',
+    'open',
+    'watch',
+  ])
 
   grunt.registerTask('default', [
   ]);
